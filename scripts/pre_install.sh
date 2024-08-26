78124d676d862d37f1f2127c76315638b81d8142
rrsoares82 <rodolforodrigues.soares@gmail.com>
2024-08-26 17:30:31 -0300

    fix pre_install.sh to install pip

--- scripts/pre_install.sh
+++ scripts/pre_install.sh
@@ -4,6 +4,9 @@ python3 --version
 
 root_path=$(dirname $(dirname $0))
 
+echo "Installing pip"
+apt install python3-pip -y
+
 echo "Installing requirements"
 python3 -m pip install -r "$root_path/requirements.txt" --break-system-packages
 
