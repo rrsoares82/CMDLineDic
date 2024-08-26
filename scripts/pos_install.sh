c24c6a998f0b2d8e49f9284ccd52c94b313d6669
rrsoares82 <rodolforodrigues.soares@gmail.com>
2024-08-26 18:05:54 -0300

    fix pos_install.sh

--- scripts/pos_install.sh
+++ scripts/pos_install.sh
@@ -2,5 +2,5 @@
 echo "pos installation checking ..."
 grep alias /etc/profile.d/00-rundict.sh
 if [ $? ]; then
-  echo alias rundict='python3 /opt/CMDLineDic/rundict.py' > /etc/profile.d/00-rundict.sh
+  echo alias rundict='"python3 /opt/CMDLineDic/rundict.py"' > /etc/profile.d/00-rundict.sh
 fi
